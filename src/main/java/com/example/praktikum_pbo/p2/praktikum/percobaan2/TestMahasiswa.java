595c0f8d353531e51d224514a572a527d8e038a7
keyfarel <carinsmddd@gmail.com>
2024-09-06 18:22:31 +0700

    tugas praktikum 2 pbo

--- src/main/java/com/example/praktikum_pbo/p2/praktikum/percobaan2/TestMahasiswa.java
+++ src/main/java/com/example/praktikum_pbo/p2/praktikum/percobaan2/TestMahasiswa.java
@@ -8,5 +8,21 @@ public class TestMahasiswa {
         mhs1.alamat = "Jl. Vinolia No 1A";
         mhs1.kelas = "2C";
         mhs1.tampilData();
+        System.out.println();
+
+        Mahasiswa mhs2 = new Mahasiswa();
+        mhs2.nim = 102;
+        mhs2.nama = "Key";
+        mhs2.alamat = "Jl. Candi VI";
+        mhs2.kelas = "2C";
+        mhs2.tampilData();
+
+        System.out.println();
+        Mahasiswa mhs3 = new Mahasiswa();
+        mhs3.nim = 103;
+        mhs3.nama = "Farel";
+        mhs3.alamat = "Jl. Raya Blitar";
+        mhs3.kelas = "2C";
+        mhs3.tampilData();
     }
 }
