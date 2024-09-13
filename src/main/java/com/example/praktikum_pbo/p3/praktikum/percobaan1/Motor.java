4fb3e7dd0633042462ee7916b0e3699caa62bc85
keyfarel <carinsmddd@gmail.com>
2024-09-13 08:40:45 +0700

    complete percobaan 2

---
+++ src/main/java/com/example/praktikum_pbo/p3/praktikum/percobaan1/Motor.java
@@ -0,0 +1,41 @@
+package com.example.praktikum_pbo.p3.praktikum.percobaan1;
+
+public class Motor {
+    private int kecepatan;
+    private boolean kontakOn = false;
+
+    public void nyalakanMesin(){
+        kontakOn = true;
+    }
+
+    public void matikanMesin(){
+        kontakOn = false;
+        kecepatan = 0;
+    }
+
+    public void tambahKecepatan (){
+        if(kontakOn){
+            kecepatan += 5;
+        }else {
+            System.out.println("Kecepatan tidak bisa bertambah karena mesin off!\n");
+        }
+    }
+
+    public void kurangiKecepatan(){
+        if(kontakOn){
+            kecepatan -= 5;
+        }else {
+            System.out.println("Kecepatan tidak bisa berkurang karena mesin off!\n");
+        }
+    }
+
+    public void printStatus() {
+        if (kontakOn == true) {
+
+            System.out.println("Kontak On");
+        } else {
+            System.out.println("Kontak Off");
+        }
+        System.out.println("Kecepatan " + kecepatan + "\n");
+    }
+}
---
+++ src/main/java/com/example/praktikum_pbo/p3/praktikum/percobaan1/MotorDemo.java
@@ -0,0 +1,21 @@
+package com.example.praktikum_pbo.p3.praktikum.percobaan1;
+
+public class MotorDemo {
+    public static void main(String[] args) {
+        Motor motor = new Motor();
+        motor.printStatus();
+        motor.tambahKecepatan();
+
+        motor.nyalakanMesin();
+        motor.printStatus();
+
+        motor.tambahKecepatan();
+        motor.printStatus();
+
+        motor.tambahKecepatan();
+        motor.printStatus();
+
+        motor.matikanMesin();
+        motor.printStatus();
+    }
+}
