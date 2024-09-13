bbf539f1249e06ccc4cc0ddd933526ed37fa6a32
keyfarel <carinsmddd@gmail.com>
2024-09-13 14:46:43 +0700

    complete tugas 6

---
+++ src/main/java/com/example/praktikum_pbo/p3/tugas/tugas4/Main.java
@@ -0,0 +1,8 @@
+package com.example.praktikum_pbo.p3.tugas.tugas4;
+
+public class Main {
+    public static void main(String[] args) {
+        var kop = new Koperasi();
+        kop.run();
+    }
+}
